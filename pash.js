4e102f23b7ff37a9c4d0b46c20671c668ac18d1e
Patrick Cool <patrickscool@gmail.com>
2021-02-07 18:46:55 -0800

    IIFE with div

--- pash.js
+++ pash.js
@@ -1,9 +1,22 @@
-var pash = function (terminal) {
-    var display = function () {
-        terminal.html("");
-    },
 
-    return {
-        display: display,
-    };
+var pash = function (terminal) {
+    var psh = document.getElementById('psh');
+    var displayLast = document.getElementsByClassName('last-command');
+    psh.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            displayLast[0].innerHTML = psh.value;
+            psh.value = '';
+        }
+    });
+
+    // var display = function (response) {
+    //     terminal.html("");
+    // }
+    // return {
+    //     display: display,
+    // };
 }
+    (function () {
+        var start = new pash;
+
+    });
