9b0bb057e6096c333a1ed550444dc96cea2f80a9
Patrick Cool <patrickscool@gmail.com>
2021-02-20 23:11:07 -0800

    remove last

--- pash.js
+++ pash.js
@@ -1,19 +1,15 @@
 
 var pash = function (terminal) {
+    var lineCount = 0;
     var psh = document.getElementById('psh');
     var displayLast = document.getElementsByClassName('last-command');
     psh.addEventListener('keypress', function (e) {
         if (e.key === 'Enter') {
-            if (displayLast[0].innerHTML !== "") {
-                var history = displayLast[0].innerHTML + "<br>";
-            }
-            displayLast[0].innerHTML = history + psh.value;
+            displayLast[0].innerHTML = `${displayLast[0].innerHTML}<p id="p${lineCount}"> ${psh.value}`;
             psh.value = '';
         }
     });
-
 }
     (function () {
         var start = new pash;
-
     });
