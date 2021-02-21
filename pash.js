670916923c43bdb423602aab3a52139e0747f753
Patrick Cool <patrickscool@gmail.com>
2021-02-20 22:23:24 -0800

    added history chain

--- pash.js
+++ pash.js
@@ -3,8 +3,9 @@ var pash = function (terminal) {
     var psh = document.getElementById('psh');
     var displayLast = document.getElementsByClassName('last-command');
     psh.addEventListener('keypress', function (e) {
+        var history = displayLast[0].innerHTML + "\n";
         if (e.key === 'Enter') {
-            displayLast[0].innerHTML = psh.value;
+            displayLast[0].innerHTML = history + psh.value;
             psh.value = '';
         }
     });
